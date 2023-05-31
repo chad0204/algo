683b17f8087e1fcf1023f84b99b24ead8d1721e8
pengchao <pengchao@lianlianpay.com>
2023-05-31 16:08:04 +0800

    feat: removeElement

--- src/main/datastruct/array_test.go
+++ src/main/datastruct/array_test.go
@@ -25,33 +25,18 @@ func removeDuplicates(nums []int) int {
 	return s + 1
 }
 
-/**
- 0 1 1 1 2 3 4 5 6 7
-
- 0 1 2 3 4 5 6 7 6 7
-             s     f
-s = 0
-f = 1
-
-s = 1
-nums[1] = nums[1]
-f = 2
-
-s = 1
-f = 3
-
-s = 1
-f = 4
-
-s = 2
-nums[2] = nums[4]
-f = 5
-
-s = 3
-nums[3] = nums[5]
-f = 6
-
-
-
-
-*/
+//删除指定元素 此题解法和上题类似
+func removeElement(nums []int, val int) int {
+	// 1 2 3 2 4 5  删除2
+	// 1 3 4 5
+	s := 0
+	f := 0
+	for f < len(nums) {
+		if nums[f] != val {
+			nums[s] = nums[f]
+			s++
+		}
+		f++
+	}
+	return s
+}
