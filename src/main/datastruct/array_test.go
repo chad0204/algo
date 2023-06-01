abfed3746030fa780b390b8fbddbf189d9064f03
pengchao <pengchao@lianlianpay.com>
2023-06-01 10:20:41 +0800

    feat: longestPalindrome

--- src/main/datastruct/array_test.go
+++ src/main/datastruct/array_test.go
@@ -69,7 +69,26 @@ func reverseString(s []byte) {
 	}
 }
 
-//最长回文子串
+//最长回文子串 这题可以动态规划(dp)
 func longestPalindrome(s string) string {
-	return ""
+	res := ""
+	for i := 0; i < len(s); i++ { //计算所有节点的中心
+		s1 := palindrome(s, i, i)   //奇数中心
+		s2 := palindrome(s, i, i+1) //偶数中心
+		if len(s1) > len(res) {
+			res = s1
+		}
+		if len(s2) > len(res) {
+			res = s2
+		}
+	}
+	return res
+}
+
+func palindrome(s string, l int, r int) string {
+	for l >= 0 && r < len(s) && s[l] == s[r] {
+		l--
+		r++
+	}
+	return s[l+1 : r] //左闭右开
 }
