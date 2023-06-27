6a395c12323597f6eb10dd5b6d90db8d17a3d773
pengchao <2389367080@qq.com>
2023-06-28 01:31:16 +0800

    feat: maxProfit

--- src/main/datastruct/dp_test.go
+++ src/main/datastruct/dp_test.go
@@ -61,10 +61,10 @@ func coinChange(coins []int, amount int) int {
 	for i := range m {
 		m[i] = -999
 	}
-	return dp(coins, amount, m)
+	return dpCoin(coins, amount, m)
 }
 
-func dp(coins []int, amount int, m []int) int {
+func dpCoin(coins []int, amount int, m []int) int {
 	if amount == 0 {
 		return 0
 	}
@@ -77,8 +77,8 @@ func dp(coins []int, amount int, m []int) int {
 
 	tmp := math.MaxInt32
 	for _, c := range coins {
-		v := dp(coins, amount-c, m) //子问题
-		if v == -1 {                // 子问题无解 父问题也无解
+		v := dpCoin(coins, amount-c, m) //子问题
+		if v == -1 {                    // 子问题无解 父问题也无解
 			continue
 		}
 		tmp = Min(tmp, v+1) // 少了c 多一枚硬币
@@ -110,7 +110,8 @@ func coinChangeIterator(coins []int, amount int) int {
 	return dp[amount]
 }
 
-/**
+/*
+*
 
 #状态
 1 持有股票 0 未持有股票
@@ -124,17 +125,15 @@ func coinChangeIterator(coins []int, amount int) int {
 #选择
 buy sell rest(无操作)
 
-
 穷举
 dp[i][j][0 or 1]
+
 	for 0 <= i < n:
 		for 1<= j <= k:
 			for s in{0, 1}:
 				dp[i][j][s] = max(buy, sell, rest)
 
 dp[n-1][k][0]
-
-
 
 #今天(i, 从0开始)没持有, 今天的交易笔数为j, dp[i][j][1]:
 1. 昨天也没持有, 今天rest, dp[i-1][j][0]
@@ -146,7 +145,6 @@ dp[i][j][0] = max{dp[i-1][j][0],  dp[i-1][j][1] + price[i]}
 2. 昨天没持有, 今天买了一笔, dp[i-1][j-1][0] - price[i]
 dp[i][j][1] = max{dp[i-1][j][1],  dp[i-1][j-1][0] - price[i]}
 
-
 base case:
 i == -1, 第一天之前
 dp[-1][x][0] = 0
@@ -156,49 +154,49 @@ j == 0, 一次交易没有
 dp[x][0][1] = ? 不存在 负无穷
 dp[x][0][0] = 0
 
-
-题目的共同约束:
+题目的共同约束, 交替买卖:
 都是不能重复买或卖, 买和卖交替执行, 才算一笔完整交易。
-1. ”你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票“
+1. ”你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票“
 2. ”你在任何时候 最多只能持有一股股票“
-3. ”你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票“
+3. ”你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）“
 
-条件3不仅要求交替买卖, 还限制只能进行一次买卖。条件2说明可以无限次买卖。但是都要求交替买卖, 否则题目就不成立。
+条件1不仅要求交替买卖, 还限制只能进行一次买卖, 且必须是不同天。 121. 买卖股票的最佳时机
+条件2没有说明次数, 可以无限次买卖, 但通过“只能持有一股“也要求交替买卖。 122. 买卖股票的最佳时机 II
+条件3直接约束了不能交替买卖, 交易次数由题目给出, k次 or 2次。123. 买卖股票的最佳时机 III; 188. 买卖股票的最佳时机 IV
 
-
-
-
+后面两种就是增加了不同的约束条件: 冷冻期, 手续费
 */
 func maxProfitK(k int, prices []int) int {
 	n := len(prices)
-	dpTable := make([][][]int, n)
-	for i := range dpTable {
-		dpTable[i] = make([][]int, k+1)
-		for j := range dpTable[i] {
-			dpTable[i][j] = make([]int, 2)
+	dp := make([][][]int, n)
+	for i := range dp {
+		dp[i] = make([][]int, k+1)
+		for j := range dp[i] {
+			dp[i][j] = make([]int, 2)
 		}
 	}
 
 	for i := 0; i < n; i++ {
 		for j := 0; j <= k; j++ {
+			//i == 0 满足状态转移方程, 通过状态转移方程推倒出用base case表示。因为-1不能作为数组下标
 			if i == 0 {
-				dpTable[i][j][0] = Max(0, math.MinInt32)
-				dpTable[i][j][1] = Max(math.MinInt32, 0-prices[i])
+				dp[i][j][0] = Max(0, math.MinInt32)
+				dp[i][j][1] = Max(math.MinInt32, 0-prices[i])
 				continue
 			}
 			if j == 0 {
-				dpTable[i][j][0] = 0
-				dpTable[i][j][1] = math.MinInt32
+				dp[i][j][0] = 0
+				dp[i][j][1] = math.MinInt32
 				continue
 			}
-			dpTable[i][j][0] = Max(dpTable[i-1][j][0], dpTable[i-1][j][1]+prices[i])
-			dpTable[i][j][1] = Max(dpTable[i-1][j][1], dpTable[i-1][j-1][0]-prices[i])
+			dp[i][j][0] = Max(dp[i-1][j][0], dp[i-1][j][1]+prices[i])
+			dp[i][j][1] = Max(dp[i-1][j][1], dp[i-1][j-1][0]-prices[i])
 		}
 	}
-	return dpTable[n-1][k][0]
+	return dp[n-1][k][0]
 }
 
-// k =1
+// k = 1
 func maxProfit1(prices []int) int {
 	/*
 	   state func:
@@ -259,6 +257,26 @@ func maxProfitInfinity(prices []int) int {
 		}
 		dp[i][0] = Max(dp[i-1][0], dp[i-1][1]+prices[i])
 		dp[i][1] = Max(dp[i-1][1], dp[i-1][0]-prices[i])
+	}
+	return dp[n-1][0]
+}
+
+// 714. 买卖股票的最佳时机含手续费
+func maxProfitWithFee(prices []int, fee int) int {
+	n := len(prices)
+	dp := make([][]int, n)
+	for i := range dp {
+		dp[i] = make([]int, 2)
+	}
+
+	for i := 0; i < n; i++ {
+		if i == 0 {
+			dp[0][0] = Max(0, math.MinInt32+prices[i])
+			dp[0][1] = -prices[i] - fee
+			continue
+		}
+		dp[i][0] = Max(dp[i-1][0], dp[i-1][1]+prices[i]) // 也可以在卖的时候给手续费, 注意还是-fee
+		dp[i][1] = Max(dp[i-1][1], dp[i-1][0]-prices[i]-fee)
 	}
 	return dp[n-1][0]
 }
