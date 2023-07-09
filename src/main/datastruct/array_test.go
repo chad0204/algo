df9a223221cdcc9edc514f1de947d1a96a45b709
pengchao <2389367080@qq.com>
2023-07-09 23:10:35 +0800

    feat: array

--- src/main/datastruct/array_test.go
+++ src/main/datastruct/array_test.go
@@ -4,28 +4,30 @@ import "testing"
 
 func TestRemoveDuplicates(t *testing.T) {
 
-	a := []int{1, 2, 2, 2, 3, 4, 5, 6, 7}
+	a := []int{2, 2, 2, 2, 3, 4, 5, 6, 7}
 
 	removeDuplicates(a)
 }
 
-// 删除有序数组中的重复项 (有序是关键)
+// 删除有序数组中的重复项 (有序是关键) 覆盖！
 func removeDuplicates(nums []int) int {
-	//0 1 1 1 2 3 4 5 6 7
-	//0 1 2 3 4 5 6 7 6 7
+	//2, 2, 2, 2, 3, 4, 5, 6, 7
+	//2, 3, 4, 5, 6, 7, 5, 6, 7
 	s := 0
 	f := 0
 	for f < len(nums) {
 		if nums[s] != nums[f] {
 			s++
 			nums[s] = nums[f]
+			f++
+		} else {
+			f++
 		}
-		f++
 	}
 	return s + 1
 }
 
-// 删除指定元素 此题解法和上题类似
+// 27. 移除元素 此题解法和上题类似
 func removeElement(nums []int, val int) int {
 	// 1 2 3 2 4 5  删除2
 	// 1 3 4 5
@@ -34,9 +36,11 @@ func removeElement(nums []int, val int) int {
 	for f < len(nums) {
 		if nums[f] != val {
 			nums[s] = nums[f]
+			f++
 			s++
+		} else {
+			f++
 		}
-		f++
 	}
 	return s
 }
