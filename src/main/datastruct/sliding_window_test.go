0fbcf99eb1969f4fb7655ffd09a194806d406421
pengchao <pengchao@lianlianpay.com>
2023-07-14 15:59:00 +0800

    feat: sliding_window

--- src/main/datastruct/sliding_window_test.go
+++ src/main/datastruct/sliding_window_test.go
@@ -6,8 +6,14 @@ import (
 	"testing"
 )
 
+/**
+
+先进窗口, 后移动游标
+
+*/
+
 func TestMinWindow(t *testing.T) {
-	fmt.Println(minWindow("ADOBECODEBANC", "ABC"))
+	fmt.Println(minWindow("a", "a"))
 }
 
 // 剑指 Offer II 017. 含有所有字符的最短字符串; 76. 最小覆盖子串
@@ -25,11 +31,10 @@ func minWindow(s string, t string) string {
 	start记录最小一次满足条件的起始位置, 因为后续的left的right还可能会满足, 会一直更新, 所以需要记录下此时的位置。只有当新的left的right比l小的时候, start才会更新成left。
 	*/
 	start := 0
-	l := math.MaxInt32
+	l := math.MaxInt32 // 不建议搞这么多变量, 定义个res := s + t就可以了
 
 	for right < len(s) {
 		add := s[right]
-		right++
 		if _, ok := needs[add]; ok {
 			//先加后判断
 			window[add] = window[add] + 1
@@ -38,6 +43,7 @@ func minWindow(s string, t string) string {
 				checkNum++
 			}
 		}
+		right++
 
 		for checkNum == len(needs) {
 			if l > (right - left) {
@@ -45,7 +51,6 @@ func minWindow(s string, t string) string {
 				start = left
 			}
 			rmv := s[left]
-			left++
 			if _, ok := needs[rmv]; ok {
 				//先判断后减
 				if window[rmv] == needs[rmv] {
@@ -54,15 +59,16 @@ func minWindow(s string, t string) string {
 				}
 				window[rmv] = window[rmv] - 1
 			}
+			left++
 		}
 	}
-	if l == math.MaxInt32 {
+	if l == math.MaxInt32 { // res == s +t
 		return ""
 	}
 	return s[start : start+l]
 }
 
-// 567. 字符串的排列; 剑指 Offer II 014. 字符串中的变位词
+// 567. 字符串的排列; 剑指 Offer II 014. 字符串中的变位词; 这题和438一样 就是返回值类型不一样
 func checkInclusion(s1 string, s2 string) bool {
 	needs := make(map[byte]int)
 	for i := range s1 {
@@ -74,26 +80,25 @@ func checkInclusion(s1 string, s2 string) bool {
 
 	for right < len(s2) {
 		add := s2[right]
-		right++
 		if _, ok := needs[add]; ok {
 			window[add] = window[add] + 1
 			if window[add] == needs[add] {
 				checkSum++
 			}
 		}
+		right++
 		for right-left == len(s1) {
 			if checkSum == len(needs) {
 				return true
 			}
 			rmv := s2[left]
-			left++
 			if _, ok := needs[rmv]; ok {
 				if window[rmv] == needs[rmv] {
 					checkSum--
 				}
 				window[rmv] = window[rmv] - 1
-
 			}
+			left++
 		}
 	}
 	return false
@@ -104,7 +109,7 @@ func TestFindAnagrams(t *testing.T) {
 	fmt.Println(ints)
 }
 
-// 438. 找到字符串中所有字母异位词; 剑指 Offer II 015. 字符串中的所有变位词
+// 438. 找到字符串中所有字母异位词; 剑指 Offer II 015. 字符串中的所有变位词; 这题和438一样 就是返回值类型不一样
 func findAnagrams(s string, p string) []int {
 
 	window := make(map[byte]int)
@@ -120,26 +125,26 @@ func findAnagrams(s string, p string) []int {
 
 	for right < len(s) {
 		add := s[right]
-		right++
 		if _, ok := needs[add]; ok {
 			window[add] = window[add] + 1
 			if window[add] == needs[add] {
 				checkSum++
 			}
 		}
+		right++
 
 		for right-left == len(p) {
 			if checkSum == len(needs) {
 				res = append(res, left)
 			}
 			rmv := s[left]
-			left++
 			if _, ok := needs[rmv]; ok {
 				if window[rmv] == needs[rmv] {
 					checkSum--
 				}
 				window[rmv] = window[rmv] - 1
 			}
+			left++
 		}
 	}
 	return res
@@ -156,14 +161,16 @@ func lengthOfLongestSubstring(s string) int {
 	left, right := 0, 0
 	for right < len(s) {
 		add := s[right]
-		right++
 		window[add] = window[add] + 1
+		right++
+
 		//出现重复项
 		for window[add] > 1 {
 			rmv := s[left]
-			left++
 			window[rmv] = window[rmv] - 1
+			left++
 		}
+
 		if right-left > l {
 			l = right - left
 		}
